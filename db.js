1066ce48a9a2f0a9bb841cfff48a0b4eb1bdf167
cdayhuff <Cdayhuff37@gmail.com>
2023-05-28 13:30:30 -0400

    new db

--- db.js
+++ db.js
@@ -11,7 +11,7 @@ const dbCredentials = {
 }
 const connectionString = 'DATABASE=bludb;' +
     'HOSTNAME=19af6446-6171-4641-8aba-9dcff8e1b6ff.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud;' +
-    'PORT=8080;' +
+    'PORT=30699;' +
     'PROTOCOL=TCPIP;' +
     'UID=yhj69774;' +
     'PWD=ZmevhWI8wQH4q1tQ;' +
