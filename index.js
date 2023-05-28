e6a085cf5142b91ee14de26616504d0a0f363071
cdayhuff <Cdayhuff37@gmail.com>
2023-05-28 12:40:12 -0400

    port

--- index.js
+++ index.js
@@ -191,6 +191,6 @@ app.delete("/chatadmin", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server listening on port 3000.");
+app.listen(8080, () => {
+  console.log("Server listening on port 8080.");
 });
